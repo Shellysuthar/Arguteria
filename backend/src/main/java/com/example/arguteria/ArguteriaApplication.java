4b0da1ce72d27ad66c0668c58e04790d62f60048
Shellysuthar <ssuthar@argusoft.com>
2023-07-15 17:54:27 +0530

    Initialized backend

---
+++ backend/src/main/java/com/example/arguteria/ArguteriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.arguteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArguteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArguteriaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/arguteria/ArguteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arguteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArguteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
